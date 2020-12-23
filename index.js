3c83456b13b12ba9db3f4b40637501fcb4ad29cf
dharmendra92-dev <dharmendrakumar.purohith@cognizant.com>
2020-12-23 03:15:11 -0800

    test commit

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use('/api/twitter', allRoutes)
 
 
 app.listen(process.env.APP_PORT, ()=>{
-    console.log("Server is listening on port: " + process.env.APP_PORT);
+    console.log("Server listening on port: " + process.env.APP_PORT);
 })
 
